7eefa95cd9db3dd2c9bba63f0be1f777ef0d1433
Alessandro <alessandromarvao@gmail.com>
2025-02-22 05:55:26 -0300

    Corrigindo erro de inicialização do ntp_config.h

--- ntp_config.h
+++ ntp_config.h
@@ -1,3 +1,6 @@
+#ifndef NTP_CONFIG_H
+#define NTP_CONFIG_H
+
 /**
  * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
  *
@@ -14,7 +17,8 @@
  #include "lwip/pbuf.h"
  #include "lwip/udp.h"
  
- typedef struct NTP_T_ {
+ typedef struct NTP_T_
+ {
      ip_addr_t ntp_server_address;
      bool dns_request_sent;
      struct udp_pcb *ntp_pcb;
@@ -30,14 +34,17 @@
  #define NTP_RESEND_TIME (10 * 1000)
  
  // Called with results of operation
- static void ntp_result(NTP_T* state, int status, time_t *result) {
-     if (status == 0 && result) {
+ static void ntp_result(NTP_T *state, int status, time_t *result)
+ {
+     if (status == 0 && result)
+     {
          struct tm *utc = gmtime(result);
          printf("got ntp response: %02d/%02d/%04d %02d:%02d:%02d\n", utc->tm_mday, utc->tm_mon + 1, utc->tm_year + 1900,
                 utc->tm_hour, utc->tm_min, utc->tm_sec);
      }
  
-     if (state->ntp_resend_alarm > 0) {
+     if (state->ntp_resend_alarm > 0)
+     {
          cancel_alarm(state->ntp_resend_alarm);
          state->ntp_resend_alarm = 0;
      }
@@ -48,14 +55,15 @@
  static int64_t ntp_failed_handler(alarm_id_t id, void *user_data);
  
  // Make an NTP request
- static void ntp_request(NTP_T *state) {
+ static void ntp_request(NTP_T *state)
+ {
      // cyw43_arch_lwip_begin/end should be used around calls into lwIP to ensure correct locking.
      // You can omit them if you are in a callback from lwIP. Note that when using pico_cyw_arch_poll
      // these calls are a no-op and can be omitted, but it is a good practice to use them in
      // case you switch the cyw43_arch type later.
      cyw43_arch_lwip_begin();
      struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, NTP_MSG_LEN, PBUF_RAM);
-     uint8_t *req = (uint8_t *) p->payload;
+     uint8_t *req = (uint8_t *)p->payload;
      memset(req, 0, NTP_MSG_LEN);
      req[0] = 0x1b;
      udp_sendto(state->ntp_pcb, p, &state->ntp_server_address, NTP_PORT);
@@ -65,41 +73,49 @@
  
  static int64_t ntp_failed_handler(alarm_id_t id, void *user_data)
  {
-     NTP_T* state = (NTP_T*)user_data;
+     NTP_T *state = (NTP_T *)user_data;
      printf("ntp request failed\n");
      ntp_result(state, -1, NULL);
      return 0;
  }
  
  // Call back with a DNS result
- static void ntp_dns_found(const char *hostname, const ip_addr_t *ipaddr, void *arg) {
-     NTP_T *state = (NTP_T*)arg;
-     if (ipaddr) {
+ static void ntp_dns_found(const char *hostname, const ip_addr_t *ipaddr, void *arg)
+ {
+     NTP_T *state = (NTP_T *)arg;
+     if (ipaddr)
+     {
          state->ntp_server_address = *ipaddr;
          printf("ntp address %s\n", ipaddr_ntoa(ipaddr));
          ntp_request(state);
-     } else {
+     }
+     else
+     {
          printf("ntp dns request failed\n");
          ntp_result(state, -1, NULL);
      }
  }
  
  // NTP data received
- static void ntp_recv(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port) {
-     NTP_T *state = (NTP_T*)arg;
+ static void ntp_recv(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port)
+ {
+     NTP_T *state = (NTP_T *)arg;
      uint8_t mode = pbuf_get_at(p, 0) & 0x7;
      uint8_t stratum = pbuf_get_at(p, 1);
  
      // Check the result
      if (ip_addr_cmp(addr, &state->ntp_server_address) && port == NTP_PORT && p->tot_len == NTP_MSG_LEN &&
-         mode == 0x4 && stratum != 0) {
+         mode == 0x4 && stratum != 0)
+     {
          uint8_t seconds_buf[4] = {0};
          pbuf_copy_partial(p, seconds_buf, sizeof(seconds_buf), 40);
          uint32_t seconds_since_1900 = seconds_buf[0] << 24 | seconds_buf[1] << 16 | seconds_buf[2] << 8 | seconds_buf[3];
          uint32_t seconds_since_1970 = seconds_since_1900 - NTP_DELTA;
          time_t epoch = seconds_since_1970;
          ntp_result(state, 0, &epoch);
-     } else {
+     }
+     else
+     {
          printf("invalid ntp response\n");
          ntp_result(state, -1, NULL);
      }
@@ -107,16 +123,19 @@
  }
  
  // Perform initialisation
- static NTP_T* ntp_init(void) {
-     NTP_T *state = (NTP_T*)calloc(1, sizeof(NTP_T));
-     if (!state) {
+ static NTP_T *ntp_init(void)
+ {
+     NTP_T *state = (NTP_T *)calloc(1, sizeof(NTP_T));
+     if (!state)
+     {
          printf("failed to allocate state\n");
          return NULL;
      }
      state->ntp_pcb = udp_new_ip_type(IPADDR_TYPE_ANY);
-     if (!state->ntp_pcb) {
+     if (!state->ntp_pcb)
+     {
          printf("failed to create pcb\n");
-         free(state);
+         free(state); // Libera memória antes de retornar
          return NULL;
      }
      udp_recv(state->ntp_pcb, ntp_recv, state);
@@ -124,12 +143,15 @@
  }
  
  // Runs ntp test forever
- void run_ntp_test(void) {
+ void run_ntp_test(void)
+ {
      NTP_T *state = ntp_init();
      if (!state)
          return;
-     while(true) {
-         if (absolute_time_diff_us(get_absolute_time(), state->ntp_test_time) < 0 && !state->dns_request_sent) {
+     while (true)
+     {
+         if (absolute_time_diff_us(get_absolute_time(), state->ntp_test_time) < 0 && !state->dns_request_sent)
+         {
              // Set alarm in case udp requests are lost
              state->ntp_resend_alarm = add_alarm_in_ms(NTP_RESEND_TIME, ntp_failed_handler, state, true);
  
@@ -142,9 +164,12 @@
              cyw43_arch_lwip_end();
  
              state->dns_request_sent = true;
-             if (err == ERR_OK) {
+             if (err == ERR_OK)
+             {
                  ntp_request(state); // Cached result
-             } else if (err != ERR_INPROGRESS) { // ERR_INPROGRESS means expect a callback
+             }
+             else if (err != ERR_INPROGRESS)
+             { // ERR_INPROGRESS means expect a callback
                  printf("dns request failed\n");
                  ntp_result(state, -1, NULL);
              }
@@ -165,3 +190,5 @@
      }
      free(state);
  }
+
+#endif
