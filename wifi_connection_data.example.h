2773ed90279ae8cbadd9aa695176c0fd3de27366
Alessandro Marvão <alessandromarvao@gmail.com>
2025-02-26 10:17:09 -0300

    Criando arquivo de exemplo para acesso à rede sem fio

---
+++ wifi_connection_data.example.h
@@ -0,0 +1,10 @@
+#ifndef WIFI_CONNECTION_DATA_H
+#define WIFI_CONNECTION_DATA_H
+
+/* ---- ATENÇÃO! ---- */
+/* - Duplique este arquivo e renomeie-o para wifi_connection_data.h - */
+/* - Altere as variáveis aqui para as suas configurações específicas - */
+#define WIFI_SSID "Nome da sua rede Wi-fi 2.5G"
+#define WIFI_PASSWORD "Senha de acesso da rede Wi-fi 2.5G"
+
+#endif
